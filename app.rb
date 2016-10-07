161f7a04e3426e481de40676504c9f77c13f05bc
FrankYan93 <yanjiadong@brandeis.edu>
2016-10-07 15:56:12 -0400

    auth success

--- app.rb
+++ app.rb
@@ -16,78 +16,81 @@ enable :sessions
 include BCrypt
 
 def create
-  newpassword = Password.create(params[:password])
-  puts newpassword
-#  @user = User.new(params[:user])
-#  @user.password=params[:password]
-#  @user.save!
-  username = params[:username]
-  puts username
-#  user = User.new(username,newpassword)
-#  user.save
-  user = User.new;
-  user.username = username
-  user.password = newpassword
-  user.save
+    newpassword = Password.create(params[:password])
+    puts newpassword
+    #  @user = User.new(params[:user])
+    #  @user.password=params[:password]
+    #  @user.save!
+    username = params[:username]
+    puts username
+    #  user = User.new(username,newpassword)
+    #  user.save
+    user = User.new
+    user.username = username
+    user.password = newpassword
+    user.save
+    session[:user_id] = user.id
 end
 
 def redirect_to_original_request
-  user = session[:user]
-#  puts "Welcome back #{user.name}."
-  original_request = session[:original_request]
-  session[:original_request] = nil
-  redirect original_request
+    user = session[:user]
+    #  puts "Welcome back #{user.name}."
+    original_request = session[:original_request]
+    session[:original_request] = nil
+    redirect original_request
 end
 
 get '/' do
-	erb :index
+    erb :index
 end
 
 get '/home' do
-  Authentication.authenticate!
-	"Welcome Home!"
-  erb :home
+    # Authentication.authenticate!
+    'Welcome Home!'
+    erb :home
 end
 
-
 post '/signup' do
-	username = params['username']
-	user = User.find_by username: username
-  @errorString=""
-	unless user.nil?
-		@errorString="Username existed!"
-		puts @errorString
-		erb :index
-	else
-    puts "create user!"
-    puts username
-    puts params[:password]
-		create
-		redirect to('/home')
-	end
+    username = params['username']
+    user = User.find_by username: username
+    @errorString = ''
+    if user.nil?
+        puts 'create user!'
+        puts username
+        puts params[:password]
+        create
+        redirect to('/home')
+    else
+        @errorString = 'Username existed!'
+        puts @errorString
+        erb :index
+    end
 end
 
 get '/signin/?' do
-  erb :signin
+    erb :signin
 end
 
 post '/signin/?' do
-  if user = User.authenticate(params)
-    session[:user] = user
-    redirect '/home'
-  else
-#    puts 'You could not be signed in. Did you enter the correct username and password?'
-    redirect '/signin'
-  end
+    current_user_id = User.authenticate(params)
+    if current_user_id.nil?
+        puts 'You could not be signed in. Did you enter the correct username and password?'
+        @errorMessage = 'You could not be signed in. Did you enter the correct username and password?'
+        redirect '/signin'
+    else
+        session[:user_id] = current_user_id
+        puts current_user_id
+        redirect '/home'
+    end
 end
 
 get '/signout' do
-  session[:user] = nil
-#  puts 'You have been signed out.'
-  redirect '/'
+    session[:user] = nil
+    #  puts 'You have been signed out.'
+    redirect '/'
 end
 
 get '/protected/?' do
-  authenticate!
-  erb :protected, locals: { title: 'Protected Page' }
+    authenticate!
+    erb :protected, locals: { title: 'Protected Page' }
 end
--- models/user.rb
+++ models/user.rb
@@ -1,55 +1,34 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
-  has_many :tweets
-  has_many :followerfollowings
-  has_many :followed_users, :through => :followerfollowings
-  has_many :followings, :class_name => "Followerfollowing", :foreign_key => :followed_user_id
-  has_many :followers, :through => :followings, :source => :user
-  has_many :notifications
-  has_many :likes
-  has_many :mentions
+    has_many :tweets
+    has_many :followerfollowings
+    has_many :followed_users, through: :followerfollowings
+    has_many :followings, class_name: 'Followerfollowing', foreign_key: :followed_user_id
+    has_many :followers, through: :followings, source: :user
+    has_many :notifications
+    has_many :likes
+    has_many :mentions
 
-  include BCrypt
-  attr_reader :name
-  attr_reader :last_signin
-
-=begin
-  def password
-    @password ||= Password.new(password)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password = @password
-    puts "give password"
-  end
-
-
-  def initialize(username)
-    @name = username
-  end
-
-  def initialize(username, password)
-    @name = username
-    self.username = username
-    self.password = password
-  end
-=end
-  def self.authenticate(config, params = {})
-    return nil if params[:username].blank? || params[:password].blank?
-
-    username = params[:username]
-    user_record = User.find_by username: username
-    return nil if user_record == nil
-
-    password_hash = Password.new(user_record[:password])
-    user_record[:id] if password_hash == params[:password]
-  end
-
-
-
-
-
-
+    include BCrypt
+    attr_reader :name
+    attr_reader :last_signin
+    #   def password
+    #     @password ||= Password.new(password)
+    #   end
+    #
+    #   def password=(new_password)
+    #     @password = Password.create(new_password)
+    #     self.password = @password
+    #     puts "give password"
+    #   end
+    def self.authenticate(params)
+        return nil if params['username'].blank? || params['password'].blank?
+        username = params[:username]
+        user_record = User.find_by username: username
+        return nil if user_record.nil?
+        puts user_record[:password]
+        password_hash = Password.new(user_record[:password])
+        user_record[:id] if password_hash == params[:password]
+    end
 end
