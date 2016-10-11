4b44f98bde8d2a15ff5badb852fcc30f32a911df
FrankYan93 <yanjiadong@brandeis.edu>
2016-10-11 19:53:14 -0400

    unfollow

--- api/users/putUnfollow.rb
+++ api/users/putUnfollow.rb
@@ -1,3 +1,4 @@
 put '/api/v1/users/:user_id/unfollow/:following_id' do
-
+  followRelation=Followerfollowing.where(followed_user_id: params[:following_id]).find_by(user_id: params[:user_id])
+  followRelation.destroy
 end
