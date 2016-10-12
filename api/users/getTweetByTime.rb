22db886f8878f4fe63a183081cb957e96691bf2a
Jiaming Xu <jmxuzju@qq.com>
2016-10-12 18:34:09 -0400

    get tweet/recent and one

---
+++ api/users/getTweetByTime.rb
@@ -0,0 +1,8 @@
+get '/api/v1/tweets/recent/' do
+  getTweetByTime
+end
+
+def getTweetByTime
+  get_tweets = Tweet.where("create_time <= ?", Date.today).order("create_time DESC").limit(100)
+  get_tweets.to_json
+end
