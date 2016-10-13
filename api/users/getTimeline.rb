e333d15e33cd22c6a821ad2d1174f54b8921622e
FrankYan93 <yanjiadong@brandeis.edu>
2016-10-12 23:51:21 -0400

    getTimeline getUserTweet

---
+++ api/users/getTimeline.rb
@@ -0,0 +1,6 @@
+get '/api/v1/users/:user_id/timeline' do
+  #Just means return recent n tweets.
+  n=20
+  join_follows_tweets = Followerfollowing.joins("JOIN tweets ON tweets.user_id = followerfollowings.followed_user_id").where(user_id: params[:user_id])
+  join_follows_tweets.merge(Tweet.order(create_time: :desc)).limit(n).select('followerfollowings.user_id,followerfollowings.followed_user_id,tweets.content,tweets.create_time').to_json
+end
