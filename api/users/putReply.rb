bc350c6a8c6c6dcaf67f5592a858902d85f68039
FrankYan93 <yanjiadong@brandeis.edu>
2016-10-13 17:24:50 -0400

    reply

--- api/users/putReply.rb
+++ api/users/putReply.rb
@@ -1,4 +1,8 @@
 put '/api/v1/users/:user_id/tweets/:tweet_id/replies/:content' do
-
-
+  newReply=Reply.new
+  newReply.user_id=params[:user_id]
+  newReply.tweet_id=params[:tweet_id]
+  newReply.content=params[:content]
+  newReply.save
+  newReply.to_json
 end
