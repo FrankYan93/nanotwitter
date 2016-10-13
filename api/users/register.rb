221852b7f4e90761c062a26ff489b038dbd22383
FrankYan93 <yanjiadong@brandeis.edu>
2016-10-13 17:53:30 -0400

    api register

---
+++ api/users/register.rb
@@ -0,0 +1,11 @@
+put '/api/v1/register/:username/:password' do
+    newUser=User.new
+    newpassword = Password.create(params[:password])
+    newUser.username=params[:username]
+    newUser.password = newpassword
+    newUser.follower_number = 0
+    newUser.followering_number = 0
+    newUser.nickname = ''
+    newUser.save
+    newUser.to_json
+end
