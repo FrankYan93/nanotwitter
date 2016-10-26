0a9e7b5ba519bdfa8ed49618487240a55d625ac2
xyjiang94 <xyjinag94@gmail.com>
2016-10-26 15:14:02 -0400

    timeline

--- service/timeline.rb
+++ service/timeline.rb
@@ -1,4 +1,4 @@
-get '/:username' do
+get '/user/:username' do
   @user_name = params[:username]
   user = User.find_by username: @user_name
   if user == nil
@@ -6,7 +6,6 @@ get '/:username' do
   else
     n = 100
     @user_tweets = Tweet.where(user_id: user[:id]).order(create_time: :desc).limit(n)
-    puts @user_tweets[0][:content]
     erb :userTimeline
   end
 end
