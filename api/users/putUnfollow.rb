6470889647a97c637234424e38f6c3ae39e12d15
FrankYan93 <yanjiadong@brandeis.edu>
2016-11-02 11:22:53 -0400

    number change

--- api/users/putUnfollow.rb
+++ api/users/putUnfollow.rb
@@ -3,6 +3,9 @@ put '/api/v1/users/:user_id/unfollow/:following_id' do
 end
 
 def heUnfollow params
-  followRelation = Followerfollowing.where(followed_user_id: params[:following_id]).find_by(user_id: params[:user_id])
-  followRelation.destroy
+  followRelation = Followerfollowing.where({followed_user_id: params[:following_id],user_id: params[:user_id]})
+  followRelation.destroy_all
+  a=User.find(params[:user_id])
+  a[:following_number]-=1
+  a.save
 end
