8a7c0de80f2ddcd3f5e7fcf27daafd10ea7614fc
FrankYan93 <yanjiadong@brandeis.edu>
2016-11-02 12:04:12 -0400

    number change

--- api/users/putUnfollow.rb
+++ api/users/putUnfollow.rb
@@ -4,11 +4,13 @@ end
 
 def heUnfollow params
   followRelation = Followerfollowing.where({followed_user_id: params[:following_id],user_id: params[:user_id]})
-  followRelation.destroy_all
-  a=User.find(params[:user_id])
-  a[:following_number]-=1
-  a.save
-  b=User.find(params[:following_id])
-  b[:follower_number]-=1
-  b.save
+  unless followRelation.size.zero?
+    followRelation.destroy_all
+    a=User.find(params[:user_id])
+    a[:following_number]-=1
+    a.save
+    b=User.find(params[:following_id])
+    b[:follower_number]-=1
+    b.save
+  end
 end
--- service/user.rb
+++ service/user.rb
@@ -1,73 +1,70 @@
-get "/:username/editProfile" do
-  @username = params[:usename]
-  currentUser=User.find_by(username: session[:username])
+get '/:username/editProfile' do
+    @username = params[:usename]
+    currentUser = User.find_by(username: session[:username])
 
-  @password_hash = Password.new (currentUser.password)
+    @password_hash = Password.new currentUser.password
 
-  @birthday = currentUser.birthday
-  @nickname = currentUser.nickname
-  @description = currentUser.description
-  erb :editProfile
+    @birthday = currentUser.birthday
+    @nickname = currentUser.nickname
+    @description = currentUser.description
+    erb :editProfile
 end
 
-post "/editProfile" do
-
-  currentUser=User.find_by(id: session[:user_id])
-  currentUser.password = Password.create(params[:password])
-  currentUser.birthday = params[:birthday] unless params[:birthday].nil?
-  currentUser.nickname = params[:nickname] unless params[:nickname].nil?
-  currentUser.description=params[:description] unless params[:description].nil?
-  currentUser.save
-  erb :home
+post '/editProfile' do
+    currentUser = User.find_by(id: session[:user_id])
+    currentUser.password = Password.create(params[:password])
+    currentUser.birthday = params[:birthday] unless params[:birthday].nil?
+    currentUser.nickname = params[:nickname] unless params[:nickname].nil?
+    currentUser.description = params[:description] unless params[:description].nil?
+    currentUser.save
+    erb :home
 end
 
-get "/:username/followers" do
-  currentUser=User.find_by(username: params[:username])
-  @user_name = params[:username]
+get '/:username/followers' do
+    currentUser = User.find_by(username: params[:username])
+    @user_name = params[:username]
 
-  @follower_number = currentUser.follower_number
-  @following_number = currentUser.following_number
+    @follower_number = currentUser.follower_number
+    @following_number = currentUser.following_number
 
-  @followers = hisFollowers(currentUser.id)
+    @followers = hisFollowers(currentUser.id)
 
-  erb :userfollowers
-
+    erb :userfollowers
 end
 
+get '/:username/followings' do
+    currentUser = User.find_by(username: params[:username])
 
-get "/:username/followings" do
+    @user_name = params[:username]
+    @follower_number = currentUser.follower_number
+    @following_number = currentUser.following_number
 
-  currentUser=User.find_by(username: params[:username])
-
-  @user_name = params[:username]
-  @follower_number = currentUser.follower_number
-  @following_number = currentUser.following_number
-
-  @followings = hisFollowings(currentUser.id)
-  @unfollowings = []
-  @count_number = 10
-  users = User.not_follow_by(currentUser.id)
-  @unfollowings = shuffle users.to_a, @count_number 
-  erb :userfollowings
+    @followings = hisFollowings(currentUser.id)
+    @unfollowings = []
+    @count_number = 10
+    users = User.not_follow_by(currentUser.id)
+    @unfollowings = shuffle users.to_a, @count_number
+    erb :userfollowings
 end
 
-get "/:username/showhome" do
-  @username = params[:username]
-  currentUser = User.find_by(username: @username)
-  @follower_number = currentUser.follower_number
-  @following_number = currentUser.following_number
+get '/:username/showhome' do
+    @username = params[:username]
+    currentUser = User.find_by(username: @username)
+    @follower_number = currentUser.follower_number
+    @following_number = currentUser.following_number
 
-  n = 100
-  @user_tweets = Tweet.where(user_id: currentUser[:id]).order(create_time: :desc).limit(n)
-  @n = params[:n].to_i || 0
+    n = 100
+    @user_tweets = Tweet.where(user_id: currentUser[:id]).order(create_time: :desc).limit(n)
+    @n = params[:n].to_i || 0
 
-  erb :showhome
+    erb :showhome
 end
 
-def iffollow id, following_id
-  relationship = Followerfollowing.where(user_id: id, following_id: following_id)
-  if relationship == nil
-    return 0
-  else return 1
-  end
+def iffollow(id, following_id)
+    relationship = Followerfollowing.where({user_id: id, followed_user_id: following_id})
+    if relationship.size.zero?
+        false
+    else
+        true
+    end
 end
