56cf6a056eea130325c27114757ea040561ae5d3
xyjiang94 <xyjinag94@gmail.com>
2016-11-02 10:22:11 -0400

    timeline&user simplify

--- service/timeline.rb
+++ service/timeline.rb
@@ -9,7 +9,7 @@ get '/:username/tweets' do
         @following_number = user.following_number
 
         n = 100
-        @user_tweets = Tweet.where(user_id: user[:id]).order(create_time: :desc).limit(n)
+        @all_tweets = Tweet.where(user_id: user[:id]).order(create_time: :desc).limit(n)
         @n = params[:n].to_i || 0
         erb :userTimeline
     end
@@ -27,32 +27,44 @@ def log_in_home
 
         n = 100
         join_follows_tweets = Followerfollowing.joins('JOIN tweets ON tweets.user_id = followerfollowings.followed_user_id').where(user_id: user_id)
-        @time_line_tweets = join_follows_tweets.merge(Tweet.order(create_time: :desc)).limit(n).select('tweets.id,tweets.nickname,tweets.username,tweets.content,tweets.create_time')
+        @all_tweets = join_follows_tweets.merge(Tweet.order(create_time: :desc)).limit(n).select('tweets.id,tweets.nickname,tweets.username,tweets.content,tweets.create_time')
         @n = params[:n].to_i || 0
         erb :home
     end
 end
 
 def not_log_in_home
-    log_in_home unless session[:user_id].nil?
-    @n = params[:n].to_i || 0
-    @all_recent_tweets = Tweet.all.order(create_time: :desc).limit(100)
-    erb :index
-  end
+    if !session[:user_id].nil?
+      log_in_home
+    else
+      @n = params[:n].to_i || 0
+      @all_tweets = Tweet.all.order(create_time: :desc).limit(100)
+      erb :index
+    end
+end
 
 def control_bar
     @page_user_name = params[:username]
+    @x = "location='/#{@page_user_name}/editProfile'"
+    @y = "location='/#{@page_user_name}/followers'"
+    @z = "location='/#{@page_user_name}/followings'"
+    @user_tweets = "location='/#{@page_user_name}/tweets'"
+
     if @page_user_name == session[:username]
-        @x = "location='/#{session[:username]}/editProfile'"
-        @y = "location='/#{session[:username]}/followers'"
-        @z = "location='/#{session[:username]}/followings'"
-        @selftimeline = "location='/#{session[:username]}/tweets'"
         erb :my_control_bar
     else
-        @x = "location='/#{@page_user_name}/editProfile'"
-        @y = "location='/#{@page_user_name}/followers'"
-        @z = "location='/#{@page_user_name}/followings'"
-        @selftimeline = "location='/#{@page_user_name}/tweets'"
         erb :other_control_bar
-  end
+    end
+end
+
+def time_line
+    for i in (0+@n*20)..(19+@n*20)
+      if @all_tweets[i] == nil
+        break
+      end
+      @tweet = @all_tweets[i]
+#      @Nickname = @all_recent_tweets[i][:nickname]
+#      puts @tweet.to_json
+      erb :singleTweet
+    end
 end
