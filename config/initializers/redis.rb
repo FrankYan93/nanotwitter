7a6233f4a33e06856c97b66748456c34a6bf1a0d
FrankYan93 <yanjiadong@brandeis.edu>
2016-11-03 13:23:39 -0400

    redis Nov.3

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,2 @@
 $redis = Redis.new(url: ENV["REDIS_URL"], timeout: 3600)
 $maxRecentNum=100
-recent_n=Tweet.all.order(create_time: :desc).limit($maxRecentNum)
-recent_n.each{|x|
-  $redis.rpush('nonlogin_timeline',x.to_json)
-}
--- service/cache_redis.rb
+++ service/cache_redis.rb
@@ -1,1 +1,5 @@
-
+recent_n=Tweet.all.order(create_time: :desc).limit($maxRecentNum)
+$redis.del 'nonlogin_timeline'
+recent_n.each{|x|
+  $redis.rpush('nonlogin_timeline',x.to_json)
+}
