851b5dcea3e59a7e3fc9fec81e1936d5d7a4e2d7
FrankYan93 <yanjiadong@brandeis.edu>
2016-11-03 13:27:25 -0400

    redis Nov.3

--- service/cache_redis.rb
+++ service/cache_redis.rb
@@ -1,5 +1,5 @@
 recent_n=Tweet.all.order(create_time: :desc).limit($maxRecentNum)
-$redis.del 'nonlogin_timeline'
+$redis.del 'nonlogin_timeline' unless $redis.nil?
 recent_n.each{|x|
   $redis.rpush('nonlogin_timeline',x.to_json)
 }
