5c534ee4d064b67899ead4b270742c113e982047
FrankYan93 <yanjiadong@brandeis.edu>
2016-11-16 15:06:29 -0500

    example of working rabbitmq rpc and local

--- sender.rb
+++ sender.rb
@@ -2,7 +2,8 @@ require 'bunny'
 conn=Bunny.new
 conn.start
 ch=conn.create_channel
-q=ch.queue('hello')
+
+q=ch.queue('task_queue'， ：durable => true)
 # ch.default_exchange.publish("Hello,World...!", :routing_key=>q.name)
 # puts "[x] Sent 'Hello World'"
 msg  = ARGV.empty? ? "Hello World!" : ARGV.join(" ")
