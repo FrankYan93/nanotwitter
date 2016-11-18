fc3dacb09d5947aea74110d1cdae67dc357e4509
FrankYan93 <yanjiadong@brandeis.edu>
2016-11-17 20:54:26 -0500

    env

--- service/notification.rb
+++ service/notification.rb
@@ -5,6 +5,7 @@ end
 get '/notification' do
   puts ENV['NANONOTI']
   uri=URI(ENV['NANONOTI']+"/api/v1/users/#{session[:user_id]}/notifications")
-  @responseArray=Net::HTTP.get(uri)  
+  @responseArray=JSON.parse Net::HTTP.get(uri)
+  @n=(params["n"]||0).to_i
   erb :notification
 end
