6db46a0683b187de4f2dade8627cb63ef4ed3ff9
xyjiang94 <xyjinag94@gmail.com>
2016-11-18 00:44:44 -0500

    json parse bug

--- api/test/reset.rb
+++ api/test/reset.rb
@@ -3,11 +3,11 @@ require 'time'
 
 # delete all data and create a testuser
 get '/api/v1/test/reset/all' do
-    thr = Thread.new {
+
       deleteAll
       resetTestuser
       user_size = User.count
-     }
+
 
     "user size = #{user_size}"
 end
