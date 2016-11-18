de394981f65f954137ab57ebafb6010c929508dd
FrankYan93 <yanjiadong@brandeis.edu>
2016-11-17 23:13:27 -0500

    modify smells

--- api/users/putRegister.rb
+++ api/users/putRegister.rb
@@ -8,10 +8,14 @@ def heRegister params
     newpassword = Password.create(params[:password])
     newUser.username = params[:username]
     newUser.password = newpassword
-    newUser.follower_number = 0
-    newUser.following_number = 0
+    newUser.follower_number = -1
+    newUser.following_number = -1
     newUser.nickname = ''
     newUser.save
+    theparam = {}
+    theparam[:user_id] = newUser[:id]
+    theparam[:following_id] = newUser[:id]
+    a_follow_b(theparam)
     newUser
   else
     "username is existed, please try another one"
