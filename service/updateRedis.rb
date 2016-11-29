5851ca94158eecacdf4f0284fe934bc7e89db6c8
xyjiang94 <xyjinag94@gmail.com>
2016-11-29 15:45:19 -0500

    add follow

--- service/updateRedis.rb
+++ service/updateRedis.rb
@@ -1,4 +1,9 @@
 #require_relative '../app.rb'
+require 'redis'
+$redis = Redis.new(url: ENV["REDIS_URL"])
+$maxRecentNum=100
+
+
 def updateUserRedis userId
   #userId=session[:user_id]
   return 1 unless $redis.ttl(userId)==-2
@@ -34,4 +39,35 @@ def updateLike userId
   hislikes.each{|x|
     $redis.sadd(label,x[:tweet_id])
   }
+end
+
+def updateFollow(userId, followedID)
+  label = userId.to_s + '_following'
+  #return 1 unless $redis.ttl(label)==-2
+  $redis.sadd(label,followedID)
+  label2 = followedID.to_s + '_follower'
+  #return 1 unless $redis.ttl(label2)==-2
+  $redis.sadd(label2,userId)
+end
+
+def updateUnFollow(userId, followedID)
+  label = userId.to_s + '_following'
+  #return 1 unless $redis.ttl(label)==-2
+  $redis.srem(label,followedID)
+  label2 = followedID.to_s + '_follower'
+  #return 1 unless $redis.ttl(label2)==-2
+  $redis.srem(label2,userId)
+end
+
+#=begin
+updateFollow(1,2)
+updateFollow(1,3)
+followings = $redis.smembers('1_following')
+puts followings.class.name
+users = User.where(:user_id => followings)
+#=end
+
+def calculateTimeline (userId)
+  following_users = $redis.smember(userId.to_s+'following')
+  #users = User.where(:user_id => )
 end
