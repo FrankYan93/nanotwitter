e5341829d35c4ee471a992934199409127c3f85c
FrankYan93 <yanjiadong@brandeis.edu>
2016-12-05 18:23:44 -0500

    reverse non-login

--- cache_redis.rb
+++ cache_redis.rb
@@ -1,5 +1,5 @@
 recent_n=Tweet.all.order(create_time: :desc).limit($maxRecentNum)
 $redis.del 'nonlogin_timeline' #unless $redis.nil?
 recent_n.each{|x|
-  $redis.lpush('nonlogin_timeline',x.to_json)
+  $redis.rpush('nonlogin_timeline',x.to_json)
 }
